f95f7ca381eb062ba12a620c915293a413b12252
Théo Fargeas <theofargeas@MacBook-Pro-de-Theo.local>
2022-10-13 14:06:24 -0700

    fix some test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const mockIntersectionObserver = class {
+    constructor() { }
+    observe() { }
+    unobserve() { }
+    disconnect() { }
+};
+
+beforeEach(() => {
+    window.IntersectionObserver = mockIntersectionObserver;
+});
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
